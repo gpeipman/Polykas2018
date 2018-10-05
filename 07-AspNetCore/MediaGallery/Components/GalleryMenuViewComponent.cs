63e2511954b75a48c331e8ff33e034273fe9cd65
Gunnar <gpeipman@hotmail.com>
2018-10-05 06:56:34 +0300

    rakenduse kosmeetika

---
+++ 07-AspNetCore/MediaGallery/Components/GalleryMenuViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediaGallery.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace MediaGallery.Components
+{
+    public class GalleryMenuViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _dataContext;
+
+        public GalleryMenuViewComponent(ApplicationDbContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<ViewViewComponentResult> InvokeAsync()
+        {
+            var folders = _dataContext.Folders
+                                      .OrderBy(f => f.Title)
+                                      .ToList();
+
+            return View("Index", folders);
+        }
+    }
+}
