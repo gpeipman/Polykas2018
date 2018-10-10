ecf4a701def939e73ddd46e3f2f7e1ced724c977
Gunnar <gpeipman@hotmail.com>
2018-10-10 14:01:04 +0300

    10. tunni keskel

---
+++ 10-AspNetCore/MediaGallery/GalleryContext.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MediaGallery.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaGallery
+{
+    public class GalleryContext
+    { 
+
+        private readonly ApplicationDbContext _dataContext;
+
+        public MediaItem CurrentItem { get; set; }
+        public string PageTitle { get; set; }
+
+        public GalleryContext(ApplicationDbContext context)
+        {
+            _dataContext = context;
+        }
+
+        public string GetFullPageTitle()
+        {
+            var title = "MediaGallery";
+
+            if(!string.IsNullOrEmpty(PageTitle))
+            {
+                title = PageTitle + " - " + title;
+            }
+
+            return title;
+        }
+
+        public int? GetCurrentFolderId()
+        {
+            if(CurrentItem == null)
+            {
+                return null;
+            }
+
+            if(CurrentItem is MediaFolder)
+            {
+                return CurrentItem.Id;
+            }
+
+            if(CurrentItem.ParentFolder == null)
+            {
+                return null;
+            }
+
+            return CurrentItem.ParentFolder.Id;
+        }
+
+        private IList<int> _currentFolderPath;
+
+        public IList<int> GetCurrentFolderPath()
+        {
+            if (_currentFolderPath != null)
+            {
+                return _currentFolderPath;
+            }
+
+            var currentId = GetCurrentFolderId();
+            if(currentId == null)
+            {
+                return new List<int>();
+            }
+
+            var folder = _dataContext.Folders
+                                     .Include(f => f.ParentFolder)
+                                     .FirstOrDefault(f => f.Id == currentId.Value);
+
+            _currentFolderPath = new List<int>();
+            
+            while(folder.ParentFolder != null)
+            {
+                _currentFolderPath.Add(folder.Id);
+
+                folder = folder.ParentFolder;
+            }
+
+            if(folder != null && folder.ParentFolder == null)
+            {
+                _currentFolderPath.Add(folder.Id);
+            }
+
+            return _currentFolderPath;
+        }
+    }
+}
